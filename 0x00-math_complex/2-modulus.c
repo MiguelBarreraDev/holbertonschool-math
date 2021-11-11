c9dd6f4af722c2e3178ba7fcfe114d02098b7565
MiguelBarreraDev <barreram987@gmail.com>
2021-11-10 22:09:54 -0500

    function that returns the moduls

--- 0x00-math_complex/2-modulus.c
+++ 0x00-math_complex/2-modulus.c
@@ -1,7 +1,15 @@
-#include "main.h"
+#include "holberton.h"
+#include <math.h>
 /**
+ * modulus - function that get the module of a complex number
  *
+ * @c: variable of type struct complex
  *
- *
+ * Return: double number
  */
+double modulus(complex c)
+{
+	printf("hola\n");
 
+	return (sqrt(pow(c.re, 2) + pow(c.im, 2)));
+}
