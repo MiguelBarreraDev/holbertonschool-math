9314ec7b40c3a1e720b2fc618b4793a0a9f150f8
MiguelBarreraDev <barreram987@gmail.com>
2021-11-10 21:31:54 -0500

    holberton.h

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,14 +6,7 @@
  */
 void display_complex_number(complex c)
 {
-	double re = c.re, im = c.im;
+	float re = c.re, im = c.im;
 
-	if ((re >= 0 && re <= 0.5) && (im >= 0 && im <= 0.5))
-		printf("0\n");
-	else if (!(re >= 0  && re <= 0.5) && !(im >= 0 && im <= 0.5))
-		printf("%.0f + %.0fi\n", re, im);
-	else if (!(re >= 0 && re <= 0.5))
-		printf("%.0f\n", re);
-	else if (!(im >= 0 && im <= 0.5))
-		printf("%.0fi\n", im);
+	printf("%f + %fi\n", re, im);
 }
