082fe08e1fea81da5978cdbfcc5869c3ca7ad505
MiguelBarreraDev <barreram987@gmail.com>
2021-11-10 20:45:56 -0500

    Add holberton.h library

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,10 +6,10 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re == 0)
+	if (c.re == 0 || c.re < 0.5)
 		printf("%.0fi\n", c.im);
-	if (c.im == 0)
+	else if (c.im == 0 || c.im < 0.5)
 		printf("%.0f\n", c.re);
-	if (c.re != 0 && c.im != 0)
+	else if (c.re != 0  && c.im != 0)
 		printf("%.0f + %.0fi\n", c.re, c.im);
 }
