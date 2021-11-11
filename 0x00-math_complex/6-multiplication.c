3ee4c3f7247bf11b334a37a2880506991c5c4be4
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 03:52:51 -0500

    Add 'multi' function for task-7 complet

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -19,3 +19,27 @@ void multiplication(complex c1, complex c2, complex *c3)
 	c3->re = (ac - bd);
 	c3->im = (ad + bc);
 }
+/**
+ * multi - function that performs the multiplication
+ * operation to complex numbers
+ *
+ * @c1: variable of type struct complex
+ * @c2: variable of type struct complex
+ * @c3: variable of type pointer to struct complex
+ *
+ * Return: variable of type struct
+ */
+complex multi(complex c1, complex c2, complex c3)
+{
+	double ac, bc, ad, bd;
+
+	ac = c1.re * c2.re;
+	bc = c1.im * c2.re;
+	ad = c1.re * c2.im;
+	bd = c1.im * c2.im;
+
+	c3.re = (ac - bd);
+	c3.im = (ad + bc);
+
+	return (c3);
+}
--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -1,7 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 /**
+ * division - function that performs the division
+ * operation to complex numbers
  *
- *
- *
+ * @c1: variable of type struct complex
+ * @c2: variable of type struct complex
+ * @c3: variable of type pointer to struct complex
  */
+void division(complex c1, complex c2, complex *c3)
+{
+	complex c2_cjd, D, d, save;
 
+	c2_cjd = conjugate(c2);
+
+	D = multi(c1, c2_cjd, save);
+	d = multi(c2, c2_cjd, save);
+
+	c3->re = (D.re / d.re);
+	c3->im = (D.im / d.re);
+}
