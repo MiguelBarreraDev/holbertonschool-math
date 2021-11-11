2576d924e8d561f4fd4a00ae30b4e552eca12f67
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 04:42:29 -0500

    add 0-display.c and 7-division.c files

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,10 +7,7 @@
 void display_complex_number(complex c)
 {
 	char i = '+';
-	float dre, dim;
 
-	dre = fmod(c.re, 1);
-	dim = fmod(c.im, 1);
 	if (c.im < 0)
 	{
 		c.im = -(c.im);
@@ -18,44 +15,20 @@ void display_complex_number(complex c)
 	}
 	if (c.re != 0 && c.im != 0)
 	{
-		if (dre == 0)
-			printf("%.0f ", c.re);
-		else
-			printf("%.1f ", c.re);
 		if (c.im == 1)
-			printf("%c i\n", i);
+			printf("%.0f %c i\n", c.re, i);
 		else
-		{
-			if (dim == 0)
-				printf("%c %.0fi\n", i, c.im);
-			else
-				printf("%c %.1fi\n", i, c.im);
-		}
+			printf("%.0f %c %.0fi\n", c.re, i, c.im);
 	}
 	else if (c.im == 0)
-	{
-		if (dre == 0)
-			printf("%.0f\n", c.re);
-		else
-			printf("%.1f\n", c.re);
-	}
+		printf("%.0f\n", c.re);
 	else if (c.re == 0)
 	{
 		if (c.im == 1)
 			printf("i\n");
 		else if (c.im > 1)
-		{
-			if (dim == 0)
-				printf("%.0f\n", c.im);
-			else
-				printf("%.1f\n", c.im);
-		}
+			printf("%.0fi\n", c.im);
 		else if (c.im < 0)
-		{
-			if (dim == 0)
-				printf("%c %.0f\n", i, c.im);
-			else
-				printf("%c %.1f\n", i, c.im);
-		}
+			printf("%c %.0fi\n", i, c.im);
 	}
 }
--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -9,13 +9,14 @@
  */
 void division(complex c1, complex c2, complex *c3)
 {
-	complex c2_cjd, D, d, save;
+	complex f1, f2;
 
-	c2_cjd = conjugate(c2);
+	f1.re = c1.re * c2.re - c1.im * c2.im * (-1);
+	f1.im = c1.im * c2.re + c2.im * (-1) * c1.re;
 
-	D = multi(c1, c2_cjd, save);
-	d = multi(c2, c2_cjd, save);
+	f2.re = c2.re * c2.re - c2.im * c2.im * (-1);
+	f2.im = c2.im * c2.re + c2.im * (-1) * c2.re;
 
-	c3->re = (D.re / d.re);
-	c3->im = (D.im / d.re);
+	c3->re = f1.re / f2.re;
+	c3->im = f1.im / f2.re;
 }
