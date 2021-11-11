1b9e9963c285a2a59e66ecb7050019a21e99f96e
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 04:47:36 -0500

    modify precision of the float

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,21 +9,15 @@ void display_complex_number(complex c)
 	double dre, dim;
 
 	if (c.im == 0)
-	{
 		printf("%.0f\n", c.re);
-	}
 	else
 	{
 		dre = fmod(c.re, 1);
 		dim = fmod(c.im, 1);
 		if (dre == 0)
-		{
 			printf("%.0f", c.re);
-		}
 		else
-		{
 			printf("%.1f", c.re);
-		}
 		if (c.im > 0)
 		{
 			printf(" + ");
@@ -33,31 +27,23 @@ void display_complex_number(complex c)
 				return;
 			}
 			if (dim == 0)
-			{
 				printf("%.0fi\n", c.im);
-			}
 			else
-			{
 				printf("%.1fi\n", c.im);
-			}
 		}
 		if (c.im < 0)
 		{
 			printf(" - ");
-			if(c.im == -1)
+			if (c.im == -1)
 			{
 				printf("i\n");
 				return;
 			}
 			printf("%.0fi\n", -c.im);
 			if (dim == 0)
-			{
 				printf("%.0fi\n", c.im);
-			}
 			else
-			{
 				printf("%.1fi\n", c.im);
-			}
 		}
 	}
 }
