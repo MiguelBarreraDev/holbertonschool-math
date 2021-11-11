019dd312c9d8dc5cfb6f3fbd7833e4ce8f727d6a
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 04:44:06 -0500

    modify precision of the float

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,7 +7,10 @@
 void display_complex_number(complex c)
 {
 	char i = '+';
+	float dre, dim;
 
+	dre = fmod(c.re, 1);
+	dim = fmod(c.im, 1);
 	if (c.im < 0)
 	{
 		c.im = -(c.im);
@@ -15,20 +18,44 @@ void display_complex_number(complex c)
 	}
 	if (c.re != 0 && c.im != 0)
 	{
-		if (c.im == 1)
-			printf("%.0f %c i\n", c.re, i);
+		if (dre == 0)
+			printf("%.0f ", c.re);
 		else
-			printf("%.0f %c %.0fi\n", c.re, i, c.im);
+			printf("%.1f ", c.re);
+		if (c.im == 1)
+			printf("%c i\n", i);
+		else
+		{
+			if (dim == 0)
+				printf("%c %.0fi\n", i, c.im);
+			else
+				printf("%c %.1fi\n", i, c.im);
+		}
 	}
 	else if (c.im == 0)
-		printf("%.0f\n", c.re);
+	{
+		if (dre == 0)
+			printf("%.0f\n", c.re);
+		else
+			printf("%.1f\n", c.re);
+	}
 	else if (c.re == 0)
 	{
 		if (c.im == 1)
 			printf("i\n");
 		else if (c.im > 1)
-			printf("%.0fi\n", c.im);
+		{
+			if (dim == 0)
+				printf("%.0f\n", c.im);
+			else
+				printf("%.1f\n", c.im);
+		}
 		else if (c.im < 0)
-			printf("%c %.0fi\n", i, c.im);
+		{
+			if (dim == 0)
+				printf("%c %.0f\n", i, c.im);
+			else
+				printf("%c %.1f\n", i, c.im);
+		}
 	}
 }
