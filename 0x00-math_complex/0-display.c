9be41153a549d996c8e89cae7caab167957b5d01
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 01:15:24 -0500

    function that performs the addition operation to complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,10 +6,41 @@
  */
 void display_complex_number(complex c)
 {
-	float re = c.re, im = c.im;
+	double dre, dim;
 
-	if (im == 0)
-		printf("%.0f\n", re);
+	if (c.im == 0)
+		printf("%.0f\n", c.re);
 	else
-		printf("%.0f + %.0fi\n", re, im);
+	{
+		dre = fmod(c.re, 1);
+		dim = fmod(c.im, 1);
+		if (dre == 0)
+			printf("%.0f", c.re);
+		else
+			printf("%.1f", c.re);
+		if (c.im > 0)
+		{
+			printf(" + ");
+			if (c.im == 1)
+				printf("i\n");
+			else if (dim == 0)
+				printf("%.0fi\n", c.im);
+			else
+				printf("%.1fi\n", c.im);
+		}
+		else if (c.im < 0)
+		{
+			printf(" - ");
+			if (c.im == -1)
+			{
+				printf("i\n");
+				return;
+			}
+			printf("%.0fi\n", -c.im);
+			if (dim == 0)
+				printf("%.0fi\n", c.im);
+			else
+				printf("%.1fi\n", c.im);
+		}
+	}
 }
--- 0x00-math_complex/4-addition.c
+++ 0x00-math_complex/4-addition.c
@@ -1,7 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 /**
+ * addition - function that performs the addition operation to complex numbers
  *
- *
- *
+ * @c1: variable of type struct complex
+ * @c2: variable of type struct complex
+ * @c3: variable of type pointer to struct complex
  */
-
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re + c2.re;
+	c3->im = c1.im + c2.im;
+}
