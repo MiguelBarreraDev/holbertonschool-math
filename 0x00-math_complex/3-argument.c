f99c3708fbc53c7907d4fe65670abbbae3586d51
MiguelBarreraDev <barreram987@gmail.com>
2021-11-10 22:21:05 -0500

    function that returns the argument of a given complex number.

--- 0x00-math_complex/3-argument.c
+++ 0x00-math_complex/3-argument.c
@@ -1,7 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 /**
+ * argument - function that get the argument of a complex number
  *
+ * @c: variable of type structu complex
  *
- *
+ * Return: double number
  */
-
+double argument(complex c)
+{
+	return (atan(c.im / c.re));
+}
