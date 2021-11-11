e058ff9079a244538971d7398b4a8ccbdc8cc508
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 01:33:49 -0500

    function that returns the conjugate of a given complex number

--- 0x00-math_complex/1-conjugate.c
+++ 0x00-math_complex/1-conjugate.c
@@ -1,7 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 /**
+ * conjugate - function that returns the conjugate of a given complex number.
  *
+ * @c: variable of type struct complex
  *
- *
+ * Return: variable of type struct complex
  */
+complex conjugate(complex c)
+{
+	c.im *= -1;
 
+	return (c);
+}
