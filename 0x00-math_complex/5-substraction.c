86c93085c4935a642aa969995e7d8b05bc668bf0
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 02:06:59 -0500

    function that performs the substraction operation to complex numbers

--- 0x00-math_complex/5-substraction.c
+++ 0x00-math_complex/5-substraction.c
@@ -1,7 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 /**
+ * substraction - function that performs the substraction
+ * operation to complex numbers
  *
- *
- *
+ * @c1: variable of type struct complex
+ * @c2: variable of type struct complex
+ * @c3: variable of type pointer to struct complex
  */
-
+void substraction(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re - c2.re;
+	c3->im = c1.im - c2.im;
+}
