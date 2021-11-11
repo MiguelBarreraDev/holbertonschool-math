9119099efd3f8daee4c50cf4d7015398d0d540d1
MiguelBarreraDev <barreram987@gmail.com>
2021-11-10 20:33:57 -0500

    Add character i the imaginary number

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,7 +7,7 @@
 void display_complex_number(complex c)
 {
 	if (c.re == 0)
-		printf("%.0f\n", c.im);
+		printf("%.0fi\n", c.im);
 	if (c.im == 0)
 		printf("%.0f\n", c.re);
 	if (c.re != 0 && c.im != 0)
