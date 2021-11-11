36dadaf3bafc5bbc65bbcd082f20aea0062f6a7d
MiguelBarreraDev <barreram987@gmail.com>
2021-11-11 03:07:07 -0500

    function that performs the multiplication operation to complex numbers

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -1,7 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 /**
+ * multiplication - function that performs the multiplication
+ * operation to complex numbers
  *
- *
- *
+ * @c1: variable of type struct complex
+ * @c2: variable of type struct complex
+ * @c3: variable of type pointer to struct complex
  */
+void multiplication(complex c1, complex c2, complex *c3)
+{
+	double ac, bc, ad, bd;
 
+	ac = c1.re * c2.re;
+	bc = c1.im * c2.re;
+	ad = c1.re * c2.im;
+	bd = c1.im * c2.im;
+
+	c3->re = (ac - bd);
+	c3->im = (ad + bc);
+}
