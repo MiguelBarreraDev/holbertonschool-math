02dd1632e04a66d7e290d8154406c3b712e3a1f2
MiguelBarreraDev <barreram987@gmail.com>
2021-11-21 21:13:00 -0500

    function that update the real and the imaginary parts of a complex number given its modulus and arguments

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -1,7 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
- *
- *
- *
- */
+* complex_from_mod_arg - Update the real and the imaginary parts
+* of a complex number given its modulus and arguments.
+* @m: modulus
+* @arg: argument
+* @c3: complex number
+*/
+void complex_from_mod_arg(double m, double arg, complex *c3)
+{
+	double x, y;
 
+	x = cos(arg) * m;
+	y = sin(arg) * m;
+
+	c3->re = x;
+	c3->im = y;
+}
