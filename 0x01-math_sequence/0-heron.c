e93840c512ecbf745dd8404d2726987db1f2fc2d
MiguelBarreraDev <barreram987@gmail.com>
2021-11-25 12:02:47 -0500

    function that return the Heron sequence

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -1,19 +1,5 @@
 #include "heron.h"
 /**
- * _sqrt - function
- * @p: number to find its square root
- *
- * Return: the square root
- */
-double _sqrt(double p)
-{
-	double t = 1;
-
-	while (t * t != p)
-		t = (t + (p / t)) / 2;
-	return (t);
-}
-/**
  * heron - function that return the Heron sequence until
  * having convergence with an error less or equal to 10^-7
  * @p: The number to find its approximate square root
@@ -36,8 +22,8 @@ t_cell *heron(double p, double x0)
 	head = new;
 	new->elt = fx;
 	new->next = NULL;
-
-	_error = (fx > _sqrt(p)) ? fx - _sqrt(p) : _sqrt(p) - fx;
+	_error = ((fx * fx) - p);
+	_error = (_error >= 0) ? _error : -(_error);
 	if (_error <= 0.0000001)
 		return (head);
 
